834be7fd0501a0f0cb3a5c4f74c77523ec009204
sohamdequantzent <sohamsdays@gmail.com>
2023-10-14 19:34:23 +0530

    experimental

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -9,9 +9,6 @@ for filename in os.listdir(profiles_dir):
             if len(lines) < 4:  # Check if the file has at least 4 lines
                 print(f"Warning: File {filename} has fewer lines than expected. Skipping...")
                 continue
-            if len(lines[0]) > 9:
-                print(f"Warning: File {filename} doesn't match the expected format. Skipping...")
-                continue
             try:
                 name = lines[1].split(":")[1].strip().replace("'", "")
                 image = lines[2].split(":")[1].strip().replace("'", "")
@@ -19,14 +16,24 @@ for filename in os.listdir(profiles_dir):
                 profiles.append((name, image, bio))
             except IndexError:
                 print(f"Warning: File {filename} doesn't match the expected format. Skipping...")
+
 with open(readme_path, 'r') as f:
     readme_content = f.read()
+
+    
+        
+          
+    
+
+        
+  
 start_placeholder = "## Contributors:\n"
 end_placeholder = "## Happy Hacking! 🚀\n"
 profile_strings = []
 for name, image, bio in profiles:
     profile_md = f"![{name}](static/images/{image})\n\n**{name}**\n\n{bio}\n\n---\n\n"
     profile_strings.append(profile_md)
-new_content = readme_content.split(start_placeholder)[0] + start_placeholder + "\n".join(profile_strings) + end_placeholder
-with open(readme_path, 'w', encoding="utf-8") as f:
+new_content = readme_content.split(start_placeholder)[0] + start_placeholder + "\n".join(profile_strings) + end_placeholder + readme_content.split(end_placeholder)[1]
+
+with open(readme_path, 'w') as f:
     f.write(new_content)
